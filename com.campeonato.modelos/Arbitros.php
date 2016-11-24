9884cd42fb67da9b7965b087b7a0c6f3bdaf757a
nataniel <nataniel@natanielsantos>
2016-11-24 17:05:56 -0200

    Testando git

---
+++ com.campeonato.modelos/Arbitros.php
@@ -0,0 +1,55 @@
+<?php
+
+class Arbitros {
+
+    private $nome;
+    private $idade;
+    private $rg;
+    private $cidade;
+    private $estado;
+
+    function __construct() {
+        
+    }
+
+    function getNome() {
+        return $this->nome;
+    }
+
+    function getIdade() {   
+        return $this->idade;
+    }
+
+    function getRg() {
+        return $this->rg;
+    }
+
+    function getCidade() {
+        return $this->cidade;
+    }
+
+    function getEstado() {
+        return $this->estado;
+    }
+
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    function setIdade($idade) {
+        $this->idade = $idade;
+    }
+
+    function setRg($rg) {
+        $this->rg = $rg;
+    }
+
+    function setCidade($cidade) {
+        $this->cidade = $cidade;
+    }
+
+    function setEstado($estado) {
+        $this->estado = $estado;
+    }
+
+}
