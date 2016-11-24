546edcf5e0c73166fcb2f51da6219ceea8e8b3f2
nataniel <nataniel@natanielsantos>
2016-11-24 16:35:33 -0200

    Testando git

--- Campeonato PI/com.campeonato.DAO/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn = mysqli_connect('localhost', 'root', '', 'mydb', '3306');
-if (!$conn) {
-    die('Erro ao conectar com o banco: ' . mysqli_connect_error());
-}else{
-    echo 'Tudo ok!';
-}
-
-//mysqli_close($conn);
